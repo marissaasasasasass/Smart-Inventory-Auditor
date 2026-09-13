7f81a2099efc3d16876b06f051a2492f46d631c7
Marissa Leong <2604758@sit.singaporetech.edu.sg>
2026-09-13 19:30:51 +0800

    Invalid Input

--- auditor.py
+++ auditor.py
@@ -6,5 +6,8 @@ while True: #Runs in a continous loop (2)
     if inventory.isdigit() and int(inventory) > 0: #Accept positive stock values as integers (3)
         inventory = int(inventory)
 
-    if inventory == 'quit':
+    elif inventory == 'quit':
         break
+
+    else: #Handle Invalid Input (4)
+        print("Invalid input. Please enter a valid stock quantity or 'quit' to exit.")  
