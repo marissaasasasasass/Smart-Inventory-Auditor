3bebb550ae4b59f1c079719159f1476fbd112e43
Marissa Leong <2604758@sit.singaporetech.edu.sg>
2026-09-14 14:40:23 +0800

    Integer Stock Values

--- auditor.py
+++ auditor.py
@@ -3,6 +3,7 @@ inventory_stock = 0
 
 while True: #Runs in a continous loop (2)
     inventory = input("Enter a stock quantity (or 'quit' to exit): ")
+    inventory = int(inventory)
 
     if inventory == "quit":
         break
