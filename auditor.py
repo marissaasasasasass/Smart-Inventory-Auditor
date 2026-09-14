33fcfc58f579024eedc6aba59e2f3879f103d6ab
Marissa Leong <2604758@sit.singaporetech.edu.sg>
2026-09-14 14:44:22 +0800

    Enforce Business Rules

--- auditor.py
+++ auditor.py
@@ -4,11 +4,15 @@ inventory_stock = 0
 while True: #Runs in a continous loop (2)
     inventory = input("Enter a stock quantity (or 'quit' to exit): ")
 
-    if inventory.isdigit(): #Accept positive stock values as integers (3)
+    if inventory.isdigit() and int(inventory) > 0: #Accept positive stock values as integers (3)
         inventory = int(inventory)
 
     elif inventory == "quit":
         break
+
+    elif inventory.isdigit() and int(inventory) <= 0: #Enforce Business Rule (5)
+        print("Stock quantity must be above 0.")
+        continue
 
     else:
         print("Invalid input. Please enter a valid stock quantity or 'quit' to exit.")
