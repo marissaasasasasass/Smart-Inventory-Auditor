a299d92e0acbadddb2f5311f0b04704b218c8f8b
Marissa Leong <2604758@sit.singaporetech.edu.sg>
2026-09-14 14:39:20 +0800

    While Loop, Quit

--- auditor.py
+++ auditor.py
@@ -1,2 +1,8 @@
 # 1. Initialize the inventory to zero in the start 
 inventory_stock = 0
+
+while True: #Runs in a continous loop (2)
+    inventory = input("Enter a stock quantity (or 'quit' to exit): ")
+
+    if inventory == "quit":
+        break
